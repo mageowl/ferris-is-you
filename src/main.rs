2c7c381e08b7408e2b83cfd0343fd78fb86ab15d
Owen L <32573897+mageowl@users.noreply.github.com>
2024-07-10 07:19:28 -0700

    hello macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::{
+    color::Color,
+    text::draw_text,
+    window::{clear_background, next_frame},
+};
+
+#[macroquad::main("Ferris is You")]
+async fn main() {
+    loop {
+        clear_background(Color::from_rgba(0, 0, 0, 255));
+        draw_text(
+            "Hello World",
+            10.0,
+            20.0,
+            24.0,
+            Color::from_rgba(255, 255, 255, 255),
+        );
+
+        next_frame().await
+    }
 }
